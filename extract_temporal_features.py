397124b7309ca06642bf9c08dd56b1a628a24bdc
purva04k <purva.kulkarni@spoken.com>
2018-02-08 16:46:49 -0500

    added a all feature function

--- extract_temporal_features.py
+++ extract_temporal_features.py
@@ -6,21 +6,6 @@
 import sys
 import csv
 
-#countUtt = 0
-#previous_end_time = 0
-#current_start_time = 0
-#NumOverlaps = 0
-#NumOfSysWrds = 0
-#NumOfSysUtt = 0
-#NumOfUsrWrds = 0
-#NumOfUsrUtt = 0
-#SysTurnDur = 0
-#UsrTurnDur = 0
-#NumOfSysTurn = 0
-#NumOfUsrTurn = 0
-#currentspkr = 0
-#TotalTalkingTime = 0
-
 def UsrTurnDur(transcript_file):
 #Calculates duration of the customer turn in the call
 	
@@ -213,28 +198,97 @@ def SysCallDom(transcript_file):
 	sys_call_dominance = float(sys_turn_dur)/total_talking_time
 	return sys_call_dominance
 
+def AllTemporalFeatures(transcript_file):
+# returns a list of all the temporal features
+# returns a list of the format: [TimeOnTask,MeanWrdsPerSysTurn,MeanWrdsPerUsrTurn,MeanSysTurnDur,NumOverlaps,UsrCallDom,SysCallDom,UsrRate,SysRate]
+	
+	count_utt = 0
+        previous_end_time = 0
+        current_start_time = 0
+        num_overlaps = 0
+        num_of_sys_wrds = 0
+        num_of_sys_utt = 0
+        num_of_usr_wrds = 0
+        num_of_usr_utt = 0
+        sys_turn_dur = 0
+        usr_turn_dur = 0
+        num_of_sys_turn = 0
+        num_of_usr_turn = 0
+        lastspkr = 0
+        currentspkr = 0
+	num_words = 0
+	total_talking_time = 0
+
+	with open(transcript_file,'r') as test_file:
+                test_csv = csv.reader(test_file, delimiter=',')
+                for line_number,line in enumerate(test_csv):
+                        speaker = line[3]
+                        currentspkr = speaker
+                        current_start_time = float(line[1])
+                        utterance = line[4]
+                        if utterance:
+                                count_utt = count_utt + 1
+                        if count_utt == 1:
+                                start_time = float(line[1])
+                        if speaker == '1':
+                                if currentspkr != lastspkr:
+                                        num_of_sys_turn = num_of_sys_turn + 1
+                                        if current_start_time < previous_end_time:
+                                                num_overlaps = num_overlaps + 1
+                                sys_turn_dur = sys_turn_dur + (float(line[2])-float(line[1]))
+                                num_of_sys_utt = num_of_sys_utt + 1
+                                num_words = len(utterance.split())
+                                num_of_sys_wrds = num_of_sys_wrds + num_words
+
+                        if speaker == '2':
+                                if currentspkr != lastspkr:
+                                        num_of_usr_turn = num_of_usr_turn + 1
+                                        if current_start_time < previous_end_time:
+                                                num_overlaps = num_overlaps + 1
+                                usr_turn_dur = usr_turn_dur + (float(line[2])-float(line[1]))
+                                num_of_usr_utt = num_of_usr_utt + 1
+                                num_words = len(utterance.split())
+                                num_of_usr_wrds = num_of_usr_wrds + num_words
+                        previous_end_time = float(line[2])
+                        lastspkr = currentspkr
+                end_time = float(line[2])
+
+	total_talking_time = usr_turn_dur + sys_turn_dur
+	time_on_task = end_time - start_time
+	mean_wrds_per_sys_turn = float(num_of_sys_wrds)/num_of_sys_turn
+	mean_wrds_per_usr_turn = float(num_of_usr_wrds)/num_of_usr_turn
+	mean_sys_turn_dur = float(sys_turn_dur)/num_of_sys_turn
+	usr_rate = float(num_of_usr_wrds)/usr_turn_dur
+	sys_rate = float(num_of_sys_wrds)/sys_turn_dur
+	usr_call_dom = float(usr_turn_dur)/total_talking_time
+	sys_call_dom = float(sys_turn_dur)/total_talking_time
+	return [time_on_task,mean_wrds_per_sys_turn,mean_wrds_per_usr_turn,mean_sys_turn_dur,num_overlaps,usr_call_dom,sys_call_dom,usr_rate,sys_rate]
 
 
-transcript_file = sys.argv[1]
-total_talking_time = TotalTalkingTime(transcript_file)
-num_overlaps = NumOverlaps(transcript_file)
-time_on_task = TimeOnTask(transcript_file)
-mean_wrds_per_sys_turn = MeanWrdsPerSysTurn(transcript_file)
-mean_wrds_per_usr_turn = MeanWrdsPerUsrTurn(transcript_file)
-mean_sys_turn_dur = MeanSysTurnDur(transcript_file)
-usr_rate = UsrRate(transcript_file)
-sys_rate = SysRate(transcript_file)
-usr_call_dominance = UsrCallDom(transcript_file)
-sys_call_dom = SysCallDom(transcript_file)
+#Use the below code to test the script
+#transcript_file = sys.argv[1]
+#total_talking_time = TotalTalkingTime(transcript_file)
+#num_overlaps = NumOverlaps(transcript_file)
+#time_on_task = TimeOnTask(transcript_file)
+#mean_wrds_per_sys_turn = MeanWrdsPerSysTurn(transcript_file)
+#mean_wrds_per_usr_turn = MeanWrdsPerUsrTurn(transcript_file)
+#mean_sys_turn_dur = MeanSysTurnDur(transcript_file)
+#usr_rate = UsrRate(transcript_file)
+#sys_rate = SysRate(transcript_file)
+#usr_call_dominance = UsrCallDom(transcript_file)
+#sys_call_dom = SysCallDom(transcript_file)
+#all_features = AllTemporalFeatures(transcript_file)
+#print all_features
 
-print "total_talking_time",total_talking_time
-print "num_overlaps", num_overlaps
-print "time_on_task", time_on_task
-print "mean_wrds_per_sys_turn", mean_wrds_per_sys_turn
-print "mean_wrds_per_usr_turn",mean_wrds_per_usr_turn
-print "mean_sys_turn_dur",mean_sys_turn_dur
-print "usr_rate", usr_rate
-print "sys_rate", sys_rate
-print "usr_call_dominance", usr_call_dominance
-print "sys_call_dom", sys_call_dom
+
+#print "total_talking_time",total_talking_time
+#print "num_overlaps", num_overlaps
+#print "time_on_task", time_on_task
+#print "mean_wrds_per_sys_turn", mean_wrds_per_sys_turn
+#print "mean_wrds_per_usr_turn",mean_wrds_per_usr_turn
+#print "mean_sys_turn_dur",mean_sys_turn_dur
+#print "usr_rate", usr_rate
+#print "sys_rate", sys_rate
+#print "usr_call_dominance", usr_call_dominance
+#print "sys_call_dom", sys_call_dom
 
